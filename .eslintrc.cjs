183d94d79efb92db808ee99a0505ff85ddac7f65
jaen <jaenfigueroa03042000@gmail.com>
2023-05-30 17:27:54 -0500

    :sparkles: feat: add editor with html css, javascript and output

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'jsx-quotes': ['warn', 'prefer-single'], //COMILLAS SIMPLE EN JSX REACT
+    'quotes': ['warn', 'single'], //USAR COMILLAS SIMPLES ''
+    'semi': ['warn', 'never'], //NO USAR PUNTO Y COMA ;
+    'indent': ['warn', 2], //USAR DOS ESPACIOS COMO TAB
+    'eqeqeq': ['warn'], //USAR SIEMPRE IGUALDAD ESTRICTA ===
+    'comma-dangle': ['warn', 'always-multiline'], //SIEMPRE PONER COMA ADICIONAL AL FINAL DE LAS LISTAS
+    // 'no-console': ['warn'], //NO DEJAR CONSOLE.LOG TIRADOS
+  },
+}
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Editor } from './components/Editor'
+
+function App() {
+  return (
+    <div className='editor'>
+      <Editor/>
+    </div>
+  )
+}
+
+export default App
